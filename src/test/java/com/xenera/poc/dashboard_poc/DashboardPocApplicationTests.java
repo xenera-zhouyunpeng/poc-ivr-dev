2b601941a9f8e16937c6886f9b456f1c4d9e48eb
zhou yunpeng <yunpeng.zhou@xenera.jp>
2024-11-15 13:47:30 +0800

    init

---
+++ src/test/java/com/xenera/poc/dashboard_poc/DashboardPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xenera.poc.dashboard_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashboardPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
