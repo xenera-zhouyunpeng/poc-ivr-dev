b8339726819c04c9c7692592e785c52cc7f2e483
zhou yunpeng <yunpeng.zhou@xenera.jp>
2024-11-15 13:45:55 +0800

    init

---
+++ src/main/java/com/xenera/poc/dashboard_poc/DashboardPocApplication.java
@@ -0,0 +1,13 @@
+package com.xenera.poc.dashboard_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DashboardPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DashboardPocApplication.class, args);
+	}
+
+}
